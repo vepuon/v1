36ffdc3ad2d746ca5f51a52e916e0c33bba60755
vepuon <vepuon@mail.ru>
2017-09-29 00:00:31 +0300

    yroc4 red2

--- script4.js
+++ script4.js
@@ -1,61 +1,26 @@
 $(document).ready(function(){
-	
-		$(".green").not().on('click', function(){
-			
-	    $(".green").css('position', 'relative').animate({'top':'-55px'}, 2000, function(){});
-		
-		$(".red").css('position', 'relative').animate({'bottom':'-55px'}, 2000, function(){});
-		
-		$(".blue").css('position', 'relative').animate({'right':'-55px'}, 2000, function(){});
-		
-		 $(".yellow").css('position', 'relative').animate({'left':'-55px'}, 2000, function(){});
-	     });
-		 
-		 
-		 
-		 
-		 
-		 
-		 $(".red").not().on('click', function(){
-			 
-	    $(".green").css('position', 'relative').animate({'top':'-55px'}, 2000, function(){});
-		
-		$(".red").css('position', 'relative').animate({'bottom':'-55px'}, 2000, function(){});
-		
-		$(".blue").css('position', 'relative').animate({'right':'-55px'}, 2000, function(){});
-		
-		 $(".yellow").css('position', 'relative').animate({'left':'-55px'}, 2000, function(){});
-	     });
-		 
-		 
-		 
-		 
-		 
-		 $(".blue").not().on('click', function(){
-			 
-	    $(".green").css('position', 'relative').animate({'top':'-55px'}, 2000, function(){});
-		
-		$(".red").css('position', 'relative').animate({'bottom':'-55px'}, 2000, function(){});
-		
-		$(".blue").css('position', 'relative').animate({'right':'-55px'}, 2000, function(){});
-		
-		 $(".yellow").css('position', 'relative').animate({'left':'-55px'}, 2000, function(){});
-	     });
-		 
-		 
-		 
-		 
-		 $(".yellow").not().on('click', function(){
-			 
-	    $(".green").css('position', 'relative').animate({'top':'-55px'}, 2000, function(){});
-		
-		$(".red").css('position', 'relative').animate({'bottom':'-55px'}, 2000, function(){});
-		
-		$(".blue").css('position', 'relative').animate({'right':'-55px'}, 2000, function(){});
-		
-		 $(".yellow").css('position', 'relative').animate({'left':'-55px'}, 2000, function(){});
-	     });
-	
-	
-		
+	$(".green").on('click', function(){
+		$(".green").animate({'top':'-0px'}, 2000, function(){});
+		$(".red").animate({'bottom':'-0px'}, 2000, function(){});
+		$(".blue").animate({'right':'-0px'}, 2000, function(){});
+		$(".yellow").animate({'left':'-0px'}, 2000, function(){});
+	});
+	$(".red").on('click', function(){
+		$(".green").animate({'top':'-0px'}, 2000, function(){});
+		$(".red").animate({'bottom':'-0px'}, 2000, function(){});
+		$(".blue").animate({'right':'-0px'}, 2000, function(){});
+		$(".yellow").animate({'left':'-0px'}, 2000, function(){});
+	});
+	$(".blue").on('click', function(){
+		$(".green").animate({'top':'-0px'}, 2000, function(){});
+		$(".red").animate({'bottom':'-0px'}, 2000, function(){});
+		$(".blue").animate({'right':'-0px'}, 2000, function(){});
+		$(".yellow").animate({'left':'-0px'}, 2000, function(){});
+	});
+	$(".yellow").on('click', function(){
+		$(".green").animate({'top':'-0px'}, 2000, function(){});
+		$(".red").animate({'bottom':'-0px'}, 2000, function(){});
+		$(".blue").animate({'right':'-0px'}, 2000, function(){});
+		$(".yellow").animate({'left':'-0px'}, 2000, function(){});
+	});
 });

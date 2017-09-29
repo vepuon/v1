18b71f254f2fa6e2ebd4440c591d955ce85f51ec
vepuon <vepuon@mail.ru>
2017-09-29 20:00:52 +0300

    yroc4 izmen3

--- script4.js
+++ script4.js
@@ -1,23 +1,5 @@
 $(document).ready(function(){
-	$(".green").on('click', function(){
-		$(".green").animate({'top':'-0px'}, 2000, function(){});
-		$(".red").animate({'bottom':'-0px'}, 2000, function(){});
-		$(".blue").animate({'right':'-0px'}, 2000, function(){});
-		$(".yellow").animate({'left':'-0px'}, 2000, function(){});
-	});
-	$(".red").on('click', function(){
-		$(".green").animate({'top':'-0px'}, 2000, function(){});
-		$(".red").animate({'bottom':'-0px'}, 2000, function(){});
-		$(".blue").animate({'right':'-0px'}, 2000, function(){});
-		$(".yellow").animate({'left':'-0px'}, 2000, function(){});
-	});
-	$(".blue").on('click', function(){
-		$(".green").animate({'top':'-0px'}, 2000, function(){});
-		$(".red").animate({'bottom':'-0px'}, 2000, function(){});
-		$(".blue").animate({'right':'-0px'}, 2000, function(){});
-		$(".yellow").animate({'left':'-0px'}, 2000, function(){});
-	});
-	$(".yellow").on('click', function(){
+	$(".ami").on('click', function(){
 		$(".green").animate({'top':'-0px'}, 2000, function(){});
 		$(".red").animate({'bottom':'-0px'}, 2000, function(){});
 		$(".blue").animate({'right':'-0px'}, 2000, function(){});
